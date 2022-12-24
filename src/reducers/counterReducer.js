999ebf24ae531edae4134701bf8dd4a8758eafdf
Bhushanab159 <106249755+Bhushanab159@users.noreply.github.com>
2022-12-24 16:40:53 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,13 @@
 const counterReducer = (state,action) =>{
-
+    switch (action) {
+        case incrementing:
+            return state + 1 ; 
+        case decrementing :
+            return state - 1 ;
+    
+        default:
+            return state;
+    }
 }
 
 export {counterReducer}
