7de1bd5b414ad898e6af3ff96205c21d1bb0b7e3
Bhushanab159 <106249755+Bhushanab159@users.noreply.github.com>
2022-12-24 16:41:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React, { useReducer } from 'react'
 import { counterReducer } from '../reducers/counterReducer';
 import '../styles/App.css';
+
+
 const App = () => {
-const [state,dispatch] = useReducer(counterReducer,{/*initial state to be placed here*/})
+const [state,dispatch] = useReducer(counterReducer, 0)
   return (
     <div id="main">
-
+        <span className="counter">{state}</span>
+        <button id='increment-btn' onClick={()=> dispatch(incrementing)} >increase</button>
+        <button id='decrement-btn' onClick={()=> dispatch(decrementing )} > decrease </button>
     </div>
   )
 }
